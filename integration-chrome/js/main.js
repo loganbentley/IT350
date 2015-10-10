de5539cf5ce3f4e16629020794c8538804a687cf
Timothy Wood <mail@woodtimothy.com>
2015-10-09 21:33:07 -0600

    Javascript click event works, pops up alert()

--- integration-chrome/js/main.js
+++ integration-chrome/js/main.js
@@ -2,6 +2,10 @@
  * Javascript functions for main.html
  */
 
+window.onload = function(){
+    document.getElementById("commentSubmitButton").addEventListener("click", postComment);
+}
+
 function postComment(){
     window.alert("Called postComment")
 }
