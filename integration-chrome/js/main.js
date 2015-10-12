103508ad54fec661462aae588ee1298762ed827e
Timothy Wood <mail@woodtimothy.com>
2015-10-12 10:16:42 -0600

    Minor edits to Integration Test Chrome extension files.

--- integration-chrome/js/main.js
+++ integration-chrome/js/main.js
@@ -7,5 +7,19 @@ window.onload = function(){
 }
 
 function postComment(){
-    window.alert("Called postComment")
+    var postURL = "https://taskbee.byu.edu/index.php/foo";
+    var comment = document.getElementById('commentInput').value;
+
+    var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("demo").innerHTML = xhttp.responseText;
+        }
+    }
+    xhttp.open("POST", postURL, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("comment=" + comment);
+    
+    alert("Comment Posted");
+
 }
