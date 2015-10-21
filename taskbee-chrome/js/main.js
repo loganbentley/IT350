67756a2aa8e7a86010152e8cc3e2d7ab1bace902
Timothy Wood <mail@woodtimothy.com>
2015-10-20 18:46:20 -0600

    Changes to widget appearance and behavior.

--- taskbee-chrome/js/main.js
+++ taskbee-chrome/js/main.js
@@ -4,7 +4,11 @@
 
 window.onload = function(){ //Start the accordion functionality
     $( "#accordion" ).accordion({
-        heightStyle: "fill"
+        collapsible: true, //Allow all sections to be closed simultaneously
+        active: false, //Start with all sections closed
+        animate: 100, //Speed up the animation to 100ms, default seems to be 200
+        icons: { "header": "ui-icon-blank", "activeHeader": "ui-icon-blank" }
+            //Blank icons
     }); 
 }
 
