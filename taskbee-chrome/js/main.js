312edefcd77dd2e7403757ea783606754750ba43
Timothy Wood <mail@woodtimothy.com>
2015-10-20 18:31:33 -0600

    Began Chrome Extension with a little style and JQuery UI (accordion widget).

---
+++ taskbee-chrome/js/main.js
@@ -0,0 +1,10 @@
+/* main.js
+ * Javascript functions for main.html
+ */
+
+window.onload = function(){ //Start the accordion functionality
+    $( "#accordion" ).accordion({
+        heightStyle: "fill"
+    }); 
+}
+
