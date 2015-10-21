81ae67eaf71212b194e6d4bd9736fe7228042e43
Timothy Wood <mail@woodtimothy.com>
2015-10-21 15:44:52 -0600

    Refinements and some test data to display (complete with internet and film references!).

--- taskbee-chrome/js/main.js
+++ taskbee-chrome/js/main.js
@@ -2,6 +2,18 @@
  * Javascript functions for main.html
  */
 
+/* TEST VARIABLES */
+var array_env = ["Work", "SchoolMWF", "SchoolTTh", "SocialMediaOnly"];
+var array_taskNames = ["Project Name", "Do that one thing", "JUST DO IT!"];
+var array_taskDates = ["10/25/2015", "11/1/2015", "10/21/2015"];
+var array_tasksCompleted = ["Friends for dinner...", "I'm gonna have", "friends for dinner"];
+
+/* HTML Snippits */
+
+var tasksTableHeader = "<tr><th>&nbsp;</th><th>Task</th><th>Due</th></tr>";
+
+/* Functions */
+
 window.onload = function(){ //Start the accordion functionality
     $( "#accordion" ).accordion({
         collapsible: true, //Allow all sections to be closed simultaneously
@@ -10,5 +22,58 @@ window.onload = function(){ //Start the accordion functionality
         icons: { "header": "ui-icon-blank", "activeHeader": "ui-icon-blank" }
             //Blank icons
     }); 
+    
+    populateEnvironments();
+    populateTasks();
+    populateTasksCompleted();
+    populateTaskGoals();
+    populateTimeGoals();
 }
 
+function populateEnvironments(){
+    
+    var environments = "";
+    
+    for(i = 0; i < array_env.length; i++){
+        environments += "<tr><td>" + array_env[i] + "</td>" + 
+        "<td>" + 
+        "<input type='button' class='env-button-start' onclick='' value='Start' />" +
+        "<input type='button' class='env-button-stop hidden' onclick='' value='Stop' />" +
+        "</td></tr>";
+    }
+    
+    document.getElementById("env-table").innerHTML = environments;
+}
+
+function populateTasks(){
+    var tasks = tasksTableHeader;
+    
+    for(i = 0; i < array_taskNames.length; i++){
+        tasks += "<tr><td><input type='checkbox' /></td>" + 
+        "<td>" + array_taskNames[i] + "</td>" + 
+        "<td>" + array_taskDates[i] + "</td></tr>";
+    }
+    
+    document.getElementById("tasks-table").innerHTML = tasks;
+}
+
+function populateTasksCompleted(){
+    var completed = "";
+    
+    for(i = 0; i < array_tasksCompleted.length; i++){
+        completed += "<tr><td>" + array_tasksCompleted[i] + "</td>" + 
+        "<td>" + 
+        "<input type='button' class='tasks-completed-reopen' onclick='' value='Reopen' />" + 
+        "</td></tr>";
+    }
+    
+    document.getElementById("tasks-table-completed").innerHTML = completed;
+}
+
+function populateTaskGoals(){
+    
+}
+
+function populateTimeGoals(){
+    
+}
