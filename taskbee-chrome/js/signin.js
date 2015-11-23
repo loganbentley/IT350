7ba96873e53235fad7d50e051f01d5be0940c20d
Timothy Wood <mail@woodtimothy.com>
2015-11-23 14:30:07 -0700

    Improvements...
    Extension requests tabs permission
    Improved signin code
    Signing redirects to main after success

--- taskbee-chrome/js/signin.js
+++ taskbee-chrome/js/signin.js
@@ -5,6 +5,9 @@
 /* Global Variables */
 
 var signinURL = "https://taskbee.byu.edu/index.php/login";
+var submitButton; //Can't assign value here, page has to load first
+var userField;
+var passField;
 
 /* TEST VARIABLES */
 
@@ -15,40 +18,38 @@ var signinURL = "https://taskbee.byu.edu/index.php/login";
 
 /* Functions */
 
-window.onload = function(){ //Start the accordion functionality
-    document.getElementById("signin-submit").addEventListener("click", postSignin);
+window.onload = function(){
+    submitButton = document.getElementById("signin-submit");
+    userField = document.getElementById("signin-user");
+    passField = document.getElementById("signin-pass");
+    
+    submitButton.addEventListener("click", postSignin);
 }
 
 function postSignin(){
-    alert("About to POST to " + signinURL);
-    
-    var username = document.getElementById("signin-user").value;
-    var password = document.getElementById("signin-pass").value;
-
+    var username = userField.value;
+    var password = passField.value;
 
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200) {
-            alert("Success.");
+            postSuccess();
         }
         else if (request.readyState == 4 && request.status >= 400) {
             alert("Request failed.  Status: " + request.status);
         }
         else{
-            alert("Status: " + request.status);
+            //alert("Status: " + request.status);
         }
     }
     request.open("POST", signinURL, true);
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.send("&username=" + username + "&password=" + password);
     
-
-    
-   /* var postData = "&username=" + username + "&password=" + password;
-    $.post(signinURL, postData, postSuccess);
-    */
 } 
 
 function postSuccess(){
-    alert('Success');
+    userField.value = "";
+    passField.value = "";
+    window.location = "main.html";
 }
